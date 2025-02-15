6460e11d2c398df7adab6d3875a527119a7029d4
@hexlet/cli <support@hexlet.io>
2025-02-15 23:09:12 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
+++ java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
@@ -8,6 +8,8 @@ import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeEach;
 
 class FileKVTest {
 
